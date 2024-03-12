5c753c1e7d5077070001c4a563f21ff952c8913e
Nicholas Sanaie <nsanaie@unc.edu>
2024-03-12 01:29:24 -0400

    Finished model_training method for wrapper.

--- src/models/model_framework.py
+++ src/models/model_framework.py
@@ -14,7 +14,7 @@ class Model(DistilBertPreTrainedModel):
 
 class Modeler:
     """
-    Creating, training, saving and other handling of the sentiment model.
+    Wrapper for creating, training, saving and other handling of the sentiment model.
     """
     def __init__(self, training):
 
@@ -38,5 +38,30 @@ class Modeler:
             self.device = torch.device("cpu")
         self.model = self.model.to(self.device)
 
-        # evaluation mode
+        # evaluation mode on initilization of modeler
         self.model.eval()
+    
+    def train(self, train_loader, optimizer, criterion):
+        # set to training mode
+        self.model.train()
+        # create batches (tqdm is loading bar plugin)
+        for inputs_ids, attention_mask, label in tqdm(train_loader):
+
+            # add data to device (including labels)
+            input_ids = input_ids.to(self.device)
+            attention_mask = attention_mask.to(self.device)
+            labels = labels.to(self.device)
+
+            # gradient reset
+            optimizer.zero_grad()
+
+            # get output and loss + backpropogation of loss
+            outputs = self.model(input_ids=input_ids, attention_mask=attention_mask).logits.squeeze(-1)
+            loss = criterion(outputs, labels.float())
+            loss.backward()
+
+            # omptimze? the mode. Need to look more into this.
+            optimizer.step()
+
+
+            
