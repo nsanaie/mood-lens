815569118f3419a03ebdd467a6adbfe317caa6a6
Nicholas <nsanaie@unc.edu>
2024-03-26 21:26:48 -0400

    Created the ar project for the front end

---
+++ src/mood-lens-ar/mood-lens-arUITests/mood_lens_arUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  mood_lens_arUITestsLaunchTests.swift
+//  mood-lens-arUITests
+//
+//  Created by Nicholas Sanaie on 3/26/24.
+//
+
+import XCTest
+
+final class mood_lens_arUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
